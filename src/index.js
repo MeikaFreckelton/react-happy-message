9fb7fc91b632f737496971df17cedcc8f7724c3b
MeikaFreckelton <meikafreckelton@gmail.com>
2020-10-26 14:05:54 +1100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const HappyMessage = () => {
+    let messages = ["You always pass failure on the way to success", 
+    "Positive anything is better than negative nothing", 
+    "Great things never come from comfort zones.", 
+    "Do something today that your future self will thank you for.", 
+    "Your limitation—it’s only your imagination."]
+    let randomMessage = messages[Math.floor(Math.random() * messages.length)]
+    return <h1>{randomMessage}</h1>
+}
+
+const App = () => {
+    return <HappyMessage />
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App />,
+    document.getElementById("root")
+)
